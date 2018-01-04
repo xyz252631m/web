356bd233a038c3c538864e5b3675798f333b075c
xyz252631m <xyz252631m@163.com>
2018-01-04 17:00:11 +0800

    Signed-off-by: xyz252631m <xyz252631m@163.com>

--- html/svg/inlineEdit.js
+++ html/svg/inlineEdit.js
@@ -1,6 +1,30 @@
 define(["angular"], function (angular) {
-    arbitrationEdit.$inject=["$scope","xmAjax","$stateParams","$state","dialog"];
-    function arbitrationEdit($scope,xmAjax,$stateParams,$state,dialog) {
+    arbitrationEdit.$inject = ["$scope", "xmAjax", "$stateParams", "$state", "dialog"];
+    function arbitrationEdit($scope, xmAjax, $stateParams, $state, dialog) {
+
+        $scope.activeType = 0;
+
+        $scope.getType = function (typeId) {
+            return $scope.activeType == typeId ? 'active' : "";
+        };
+
+        $scope.$watch('activeType', function (val, oldVal) {
+            console.log(val)
+        });
+
+        $scope.editStyle={
+            width:"640px",
+            height:"480px",
+            marginLeft: "-320px",
+            marginTop:"-240px"
+        };
+
+        $scope.svgStyle={
+            width:"640px",
+            height:"480px"
+        }
+
+
         // //获取数据
         // xmAjax.post(window.adminBaseUrl + "/views/selectManagerArbitrationApplicaInfoById/" + $stateParams["id"]).success(function (red) {
         //     if (red) {
@@ -16,6 +40,24 @@ define(["angular"], function (angular) {
         //     $state.go("html",{module:'associator',ctrl:'arbitration',action:'list',id:null});
         // };
     }
+
+    menuEvent.$inject = ["xmElement", "$parse"];
+    function menuEvent(xmElement, $parse) {
+        return {
+            scope: false,
+            link: function (scope, ele, attr, ctrl) {
+                ele.children().bind("click", function () {
+                    var node = this;
+                    scope.$apply(function () {
+                        scope.activeType = Array.prototype.indexOf.apply(ele.children(), [node]);
+                    });
+                });
+            }
+        }
+    }
+
     // controller
-    angular.module("webs").register.controller("inlineEdit",arbitrationEdit);
+    angular.module("webs").register.controller("inlineEdit", arbitrationEdit);
+
+    angular.module("webs").register.directive("menuEvent", menuEvent);
 });
