bb97373dc017a0854f40e7ff10cbaf9896f9627b
wangwei <wangwei@163.com>
2021-04-30 17:14:52 +0800

    “...”

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ let filePath = path.resolve();
 let querystring = require('querystring');
 let codeDesign = require('./server/codeDesign');
 
-console.log(123,codeDesign)
+console.log(123, codeDesign)
 
 let tool = require('./server/tool');
-let resultMsg =tool.result;
+let resultMsg = tool.result;
 
 //获取后缀名
 let getdir = tool.getFileExtend;
@@ -67,11 +67,12 @@ http.createServer(function (request, response) {
         // console.log("Request for " + pathname);
         //从文件系统中都去请求的文件内容
         fs.readFile(pathname.substr(1), function (err, data) {
+
             if (err) {
                 fs.readdir(path.join(filePath, pathname), function (err2, files) {
-                    if(err2){
+                    if (err2) {
                         response.writeHead(404, {'Content-Type': 'text/html'});
-                    }else{
+                    } else {
                         response.writeHead(200, {'Content-Type': 'text/html'});
                         //写会相应内容
                         if (files) {
@@ -82,27 +83,39 @@ http.createServer(function (request, response) {
                     //发送响应数据
                     response.end();
                 });
-                console.log(1236666,err);
+                console.log(1236666, err);
             } else {
+                let stat = fs.statSync(pathname.substr(1))
+                const serverTime = stat.ctime.toUTCString();
+                //读取时间
+                const clientTime = request.headers["if-modified-since"];
+                // 若是本地的文件修改时间和浏览器返回的修改时间相同，则使用缓存的数据，返回304
+                if (clientTime === serverTime) {
+                    response.statusCode = 304
+                    return response.end()
+                }
+                //强制缓存 单位: s
+                let cacheObj = {'cache-control': 'max-age=10', "Last-Modified": serverTime};
+
                 //HTTP 状态码 200 ： OK
                 //Content Type:text/plain
                 let t = getdir(pathname);
                 if (t === "html") {
-                    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...cacheObj});
                 } else if (t === "css") {
-                    response.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
+                    response.writeHead(200, {'Content-Type': 'text/css; charset=utf-8', ...cacheObj});
                 } else if (t === "jpg") {
-                    response.writeHead(200, {'Content-Type': 'image/jpeg'});
+                    response.writeHead(200, {'Content-Type': 'image/jpeg', ...cacheObj});
                 } else if (t === "png") {
-                    response.writeHead(200, {'Content-Type': 'image/png'});
+                    response.writeHead(200, {'Content-Type': 'image/png', ...cacheObj});
                 } else if (t === "js") {
-                    response.writeHead(200, {'Content-Type': 'application/javascript'});
+                    response.writeHead(200, {'Content-Type': 'application/javascript', ...cacheObj});
                 } else if (t === "woff") {
-                    response.writeHead(200, {'Content-Type': 'font/woff'});
+                    response.writeHead(200, {'Content-Type': 'font/woff', ...cacheObj});
                 } else if (t === "woff2") {
-                    response.writeHead(200, {'Content-Type': 'font/woff2'});
+                    response.writeHead(200, {'Content-Type': 'font/woff2', ...cacheObj});
                 } else {
-                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.writeHead(200, {'Content-Type': 'text/html', ...cacheObj});
                 }
                 //写会相应内容
                 response.end(data, 'binary');
@@ -118,7 +131,7 @@ http.createServer(function (request, response) {
     // })
 
 
-}).listen(80)
+}).listen(3000)
 console.log("服务启动成功")
 
 
